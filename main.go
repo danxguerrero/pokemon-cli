b8739e50c801fb265ac67ef852089ac56334db8b
danxguerrero <danxguerrero@yahpp.com>
2023-12-28 01:11:58 -0500

    Reads a file and prints XP based on the number of lines

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+    // "encoding/json"
+    "flag"
+    "fmt"
+    "os"
+    // "net/http"
+    "strings"
+    // ... other imports as needed
+)
+
 func main() {
-	
+    // Parse command-line arguments
+    filePath := flag.String("file", "", "Path to the code file to analyze")
+    flag.Parse()
+
+    // Read code file and count lines
+    file, err := os.ReadFile(*filePath)
+    if err != nil {
+        panic(err) // Replace with proper error handling
+    }
+    lines := strings.Split(string(file), "\n")
+    numLines := len(lines)
+
+    // Grant experience and trigger encounters
+    xpPerLine := 10
+    xpGained := numLines * xpPerLine
+    fmt.Printf("You gained %d XP!\n", xpGained)
+
+    encounterChance := 10
+    if numLines%encounterChance == 0 {
+        // Trigger a Pokémon encounter (we'll implement this later)
+		fmt.Println("Pokemon encounter!")
+    }
+
+    // (Coming up) Fetch Pokémon data from PokeAPI and display output
 }
