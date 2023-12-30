cb56a5199670208704e887f3186f470e1ce4de00
danxguerrero <danxguerrero@yahpp.com>
2023-12-30 04:54:40 -0500

    Random pokemon encounters can be triggered based on the lines of code in the file

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    // "encoding/json"
-    "flag"
-    "fmt"
-    "os"
-    // "net/http"
-    "strings"
-    // ... other imports as needed
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"math/rand"
+	"strings"
 )
 
 func main() {
@@ -30,9 +30,47 @@ func main() {
 
     encounterChance := 10
     if numLines%encounterChance == 0 {
-        // Trigger a Pokémon encounter (we'll implement this later)
-		fmt.Println("Pokemon encounter!")
+        encounterPokemon(xpGained)
     }
 
     // (Coming up) Fetch Pokémon data from PokeAPI and display output
+}
+
+func fetchRandomPokemon(xp int) struct {
+	Name string `json: "name"`
+	Url string `json: "url"`}{
+	url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon?offset=%d&limit=10", xp%100)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	var pokemonResults struct {
+		Results []struct {
+			Name string `json: "name"`
+			Url string `json: "url"`
+		} `json: "results"`
+	}
+	err = json.NewDecoder(res.Body).Decode(&pokemonResults)
+	if err != nil {
+		panic(err)
+	}
+
+	randomIndex := rand.Intn(len(pokemonResults.Results))
+	randomPokemon := pokemonResults.Results[randomIndex]
+
+	return randomPokemon
+}
+
+func encounterPokemon(xp int) {
+	randomPokemon := fetchRandomPokemon(xp)
+
+	fmt.Println("A wild Pokemon appears")
+	fmt.Printf("It's a %s!\n", randomPokemon.Name)
 }
